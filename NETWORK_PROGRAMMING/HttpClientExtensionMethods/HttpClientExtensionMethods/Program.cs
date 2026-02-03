14e6eb5c14f4a49e078f9c4482976519b548c38a
Matteo <matteo.parrella@issgreppi.it>
2026-02-03 08:25:53 +0100

    aggiunto progetto esercizio

---
+++ NETWORK_PROGRAMMING/HttpClientExtensionMethods/HttpClientExtensionMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿// Example of using HttpClient extension methods to send and receive JSON data.
+//https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/how-to
+using System.Net.Http.Json;
+namespace HttpClientExtensionMethods
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+    }
+    public class Program
+    {
+        public static async Task Main()
+        {
+            HttpClient client = new()
+            {
+                BaseAddress = new Uri("https://jsonplaceholder.typicode.com")
+            };
+            // Get the user information.
+            User? user = await client.GetFromJsonAsync<User>("users/1");
+            Console.WriteLine($"Id: {user?.Id}");
+            Console.WriteLine($"Name: {user?.Name}");
+            Console.WriteLine($"Username: {user?.Username}");
+            Console.WriteLine($"Email: {user?.Email}");
+            // Post a new user.
+            HttpResponseMessage response = await client.PostAsJsonAsync("users", user);
+            Console.WriteLine(
+            $"{(response.IsSuccessStatusCode ? "Success" : "Error")} - {response.StatusCode}");
+        }
+    }
+}
+// Produces output like the following example but with different names:
+// Id: 1
+// Name: Leanne Graham
+// Username: Bret
+// Email: Sincere @april.biz
+// Success - Created
---
+++ NETWORK_PROGRAMMING/JsonDemo1/JsonDemo1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.Json;
+//esempio di serializzazione da oggetti .NET a oggetti JSON
+// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/how-to
+namespace JSONDemo1
+{
+    //il modello dei dati
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string? Summary { get; set; }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //versione sincrona
+            //JSONDemoEsempio1();
+            //Versione asincrona
+            await JSONDemoAsyncEsempio1();
+        }
+        private static void JSONDemoEsempio1()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+            var weatherForecast2 = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-02"),
+                TemperatureCelsius = 30,
+                Summary = "Very Hot"
+            };
+            List<WeatherForecast> previsioni = new() { weatherForecast, weatherForecast2 };
+            //serializzazione: da oggetto .NET a oggetto JSON
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(weatherForecast, options);
+            Console.WriteLine(jsonString);
+            //serializzazione di una collection in JSON
+            Console.WriteLine();
+            Console.WriteLine();
+            string jsonCollectionString = JsonSerializer.Serialize(previsioni, options);
+            Console.WriteLine(jsonCollectionString);
+            //salviamo su file la collection
+            string fileName = "WeatherForecasts.json";
+            File.WriteAllText(fileName, jsonCollectionString);
+            //leggo dal file e stampo a console
+            Console.WriteLine("Lettura del JSON da file");
+            Console.WriteLine(File.ReadAllText(fileName));
+        }
+        private static async Task JSONDemoAsyncEsempio1()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+            var weatherForecast2 = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-02"),
+                TemperatureCelsius = 30,
+                Summary = "Very Hot"
+            };
+            List<WeatherForecast> previsioni = new() { weatherForecast, weatherForecast2 };
+            //serializzazione: da oggetto .NET a oggetto JSON
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(weatherForecast, options);
+            Console.WriteLine(jsonString);
+            //serializzazione di una collection in JSON
+            Console.WriteLine();
+            Console.WriteLine();
+            string jsonCollectionString = JsonSerializer.Serialize(previsioni, options);
+            Console.WriteLine(jsonCollectionString);
+            //salviamo su file la collection
+            string fileName = "WeatherForecasts.json";
+            FileStream fileStream = File.Create(fileName);
+            //Impostiamo il flusso di serializzazione JSON direttamente sullo stream che punta al file
+            await JsonSerializer.SerializeAsync(fileStream, previsioni, options);
+            await fileStream.DisposeAsync();
+            //File.WriteAllText(fileName, jsonCollectionString);
+            //leggo dal file e stampo a console
+            Console.WriteLine("Lettura del JSON da file");
+            Console.WriteLine(await File.ReadAllTextAsync(fileName));
+        }
+    }
+}
